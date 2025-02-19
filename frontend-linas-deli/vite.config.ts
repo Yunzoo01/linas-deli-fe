92d054386c9726904f371d0ca17438be60d96f33
Yunzoo01 <joyunzoooo@gmail.com>
2025-02-18 23:09:09 -0800

    add TailwindCSS

--- frontend-linas-deli/vite.config.ts
+++ frontend-linas-deli/vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',  // Spring Boot 백엔드 URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
