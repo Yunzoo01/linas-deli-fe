7d6e0c31144ad01b4f278798cbdc23f97770dd26
Yunzoo01 <joyunzoooo@gmail.com>
2025-02-27 19:11:45 -0800

    #26 test of 26 branch

--- frontend-linas-deli/src/pages/Menu.tsx
+++ frontend-linas-deli/src/pages/Menu.tsx
@@ -1,6 +1,6 @@
 const Menu = () => {
     return (
-      <div className="container mx-auto p-6">
+      <div className="container mx-auto p-6 ">
         <h1 className="text-4xl font-bold text-gray-900">Menu</h1>
       </div>
     );
