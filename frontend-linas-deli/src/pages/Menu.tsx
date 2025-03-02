eb1a65ddd613aea13ebf481c8ce74d38d80ff00f
Yunzoo01 <joyunzoooo@gmail.com>
2025-03-02 01:34:46 -0800

    delete

--- frontend-linas-deli/src/pages/Menu.tsx
+++
@@ -1,9 +0,0 @@
-const Menu = () => {
-    return (
-      <div className="container mx-auto p-6 ">
-        <h1 className="text-4xl font-bold text-gray-900">Menu</h1>
-      </div>
-    );
-  };
-  
-  export default Menu;
